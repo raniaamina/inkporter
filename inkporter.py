d8157ba67835552b6bdaf20263674be7c9e08d1a
Sofyan Sugianto <sofyanartem@gmail.com>
2020-04-05 16:19:52 +0700

    add some functionality: jpg and svg

--- inkporter.py
+++ inkporter.py
@@ -4,8 +4,10 @@ import inkex
 inkex.localize()
 
 import os
+import shutil
 import sys
 import simplestyle
+import tempfile
 
 
 class Inkporter(inkex.Effect):
@@ -33,38 +35,47 @@ class Inkporter(inkex.Effect):
                                     type="int", action="store", dest="quality",
                                     help="Quality of image export, 0-100, higher better but slower",  default=100)
         # temporary svg out file
-        self.format_options = {
-            "jpg": self.do_jpg,
-            "pdf": self.do_pdf,
-            "svg": self.do_svg,
-            "eps": self.do_eps,
-            "booklet": self.do_booklet,
-        }
-        self.svgout = {}
+        self.tmpdir = tempfile.mkdtemp(prefix="inkporter")
+        self.tmpout = []
 
     def do_jpg(self):
         if not self.has_imagemagick():
-            inkex.errmsg("Please install Imagemagick to do JPG/Booklet export")
+            inkex.debug("Please install Imagemagick to do JPG/Booklet export")
             return
-        pass
+        for item in self.selected:
+            tmpfile_export = self.tmpdir + "/" + item + ".png"
+            command = "inkscape -z -j -i '%s' -e %s -d %d -f %s @>/dev/null" % (item, tmpfile_export, self.options.dpi, os.path.abspath(self.svg_file))
+            os.system(command)
+            self.tmpout.append(tmpfile_export)
+            command2 = "convert '%s' -background '%s' -flatten -quality %d -colorspace CMYK '%s'" % (tmpfile_export, 
+                        self.options.bg_color, 
+                        self.options.quality, 
+                        os.path.expandvars(self.options.output_dir) + "/" + item + ".jpg")
+            os.system(command2)
 
     def do_pdf(self):
+        # TODO
         if not self.has_ghostscript():
-            inkex.errmsg("Please install Ghostscript to do PDF export")
+            inkex.debug("Please install Ghostscript to do PDF export")
             return
-        pass
+        command = ""
 
     def do_svg(self):
-        pass
+        for item in self.selected:
+            file_export = os.path.expandvars(self.options.output_dir) + "/" + item + ".svg"
+            command = "inkscape -z -j -i '%s' -l '%s' -f %s @>/dev/null" % (item, file_export, os.path.abspath(self.svg_file))
+            os.system(command)
 
     def do_eps(self):
-        pass
+        # TODO
+        command = ""
 
     def do_booklet(self):
+        # TODO
         if not self.has_imagemagick():
-            inkex.errmsg("Please install Imagemagick to do JPG/Booklet export")
+            inkex.debug("Please install Imagemagick to do JPG/Booklet export")
             return
-        pass
+        command = ""
 
     def has_ghostscript(self):
         status, output = self.get_cmd_output('gs --help')
@@ -73,6 +84,10 @@ class Inkporter(inkex.Effect):
     def has_imagemagick(self):
         status, output = self.get_cmd_output('convert --version')
         return status == 0 and 'ImageMagick' in output
+    
+    def has_rsvg(self):
+        status, output = self.get_cmd_output('rsvg-convert --version')
+        return status == 0 and 'rsvg-convert' in output
 
     def get_cmd_output(self, cmd):
         # Adapted from webslicer extension (extensions > web > slicer)
@@ -87,9 +102,36 @@ class Inkporter(inkex.Effect):
             text = text[:-1]
         return sts, text
 
+    def do_cleanup(self):
+        for item in self.tmpout:
+            if os.path.exists(os.path.abspath(item)):
+                os.remove(os.path.abspath(item))
+        if os.path.isdir(self.tmpdir):
+            os.rmdir(self.tmpdir)
+
     # called when extension is running
     def effect(self):
-        inkex.debug(self.options)
+        if len(self.selected) < 1 or len(self.options.id) < 1:
+            inkex.errmsg("Please select at least 1 object or id to use this extension!")
+            return
+        try:
+            if not os.path.isdir(self.options.output_dir):
+                os.mkdir(os.path.expandvars(self.options.output_dir))
+            if self.options.format == "jpg":
+                self.do_jpg()
+            elif self.options.format == "pdf":
+                self.do_pdf()
+            elif self.options.format == "svg":
+                self.do_svg()
+            elif self.options.format == "eps":
+                self.do_eps()
+            elif self.options.format == "booklet":
+                self.do_booklet()
+            self.do_cleanup()
+        except Exception as e:
+            import traceback
+            inkex.debug(traceback.print_exc())
+            inkex.debug(e)
 
 if __name__ == '__main__':
     e = Inkporter()
