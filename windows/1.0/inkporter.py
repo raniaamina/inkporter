887778115a78e31dd279f1b72e25213f19bd3a3d
RJ95 <48236174+maslanangdev@users.noreply.github.com>
2020-05-11 22:10:10 +0700

    Add files via upload

---
+++ windows/1.0/inkporter.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+
+import tempfile
+import re
+from lxml import etree
+import sys
+import shutil
+import os
+import inkex
+from time import sleep
+
+__version__ = '1.1.0'
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+class Inkporter(inkex.Effect):
+    def __init__(self, *args, **kwargs):
+        inkex.Effect.__init__(self, *args, **kwargs)
+        try:
+            self.tty = open("/dev/tty", 'w')
+        except:
+            # '/dev/null' for POSIX, 'nul' for Windows.
+            self.tty = open(os.devnull, 'w')
+            # print >>self.tty, "gears-dev " + __version__
+        self.arg_parser.add_argument('--tab')
+        self.arg_parser.add_argument("-f", "--format",
+                                     type=str, dest="format",
+                                     help="Preferred output format", default="png")
+        self.arg_parser.add_argument("--id-pattern",
+                                     type=str, dest="id_pattern",
+                                     help="IDs to export")
+        self.arg_parser.add_argument("--with-cmyk",
+                                     type=inkex.Boolean, dest="with_cmyk",
+                                     help="Use CMYK instead of RGB (for JPG/PDF)")
+        self.arg_parser.add_argument("--dpi",
+                                     type=int, dest="dpi",
+                                     help="DPI for bitmap image output format", default=96)
+        self.arg_parser.add_argument("-D", "--output-dir",
+                                     type=str, dest="output_dir",
+                                     help="Destination folder for saving your exports", default=os.path.expanduser("~"))
+        self.arg_parser.add_argument("--bg-color",
+                                     type=str, dest="bg_color",
+                                     help="Background color for JPG Export", default="white")
+        self.arg_parser.add_argument("-q", "--quality",
+                                     type=int, dest="quality",
+                                     help="Quality of image export, 0-100, higher better but slower",  default=100)
+        # temporary svg out file
+        self.id_to_process = None
+        self.tmpdir = tempfile.mkdtemp(prefix="inkporter")
+        self.tmplog_fd, self.tmplog_path = tempfile.mkstemp(prefix="inkporter", suffix=".log")
+        self.tmpout = []
+    
+    def do_png(self):
+        file_export = '"' + self.options.output_dir + '"'
+        command = "start inkporter_ext png {0} {1} {2} {3} {4}".format(
+            self.myfile, self.options.id_pattern, file_export, self.options.dpi, self.options.bg_color)
+        os.system(command)
+
+    def do_jpg(self):
+        file_export = '"' + self.options.output_dir + '"'
+        
+        options = " RGB"
+        if self.options.with_cmyk:
+            options = "CMYK"
+            
+        command = "start inkporter_ext jpeg {0} {1} {2} {3} {4} {5} {6}".format(
+            self.myfile, self.options.id_pattern, file_export, self.options.dpi, self.options.bg_color, self.options.quality, options)
+        os.system(command)
+
+    def do_pdf(self):
+        file_export = '"' + self.options.output_dir + '"'
+        if self.options.with_cmyk:
+            if not self.has_ghostscript():
+                inkex.utils.errormsg("Please install Ghostscript to do PDF export")
+                return
+            command = "start inkporter_ext pdf_cmyk {0} {1} {2}".format(
+                self.myfile, self.options.id_pattern, file_export)
+            os.system(command)
+        else:
+            command = "start inkporter_ext pdf {0} {1} {2}".format(
+                self.myfile, self.options.id_pattern, file_export)
+            os.system(command)
+        os.close(self.tmplog_fd)
+
+    def do_svg(self):
+        file_export = '"' + self.options.output_dir + '"'
+        command = "start inkporter_ext svg {0} {1} {2}".format(
+            self.myfile, self.options.id_pattern, file_export)
+        os.system(command)
+        os.close(self.tmplog_fd)
+
+    def do_eps(self):
+        file_export = '"' + self.options.output_dir + '"'
+        command = "start inkporter_ext eps {0} {1} {2}".format(
+            self.myfile, self.options.id_pattern, file_export)
+        os.system(command)
+        os.close(self.tmplog_fd)
+
+    def do_booklet(self):
+        if self.options.with_cmyk:
+            file_export = '"' + self.options.output_dir + '"'
+            command = "start inkporter_ext booklet_cmyk {0} {1} {2}".format(
+                self.myfile, self.options.id_pattern, file_export)
+            os.system(command)
+            os.close(self.tmplog_fd)
+        else:
+            file_export = '"' + self.options.output_dir + '"'
+            command = "start inkporter_ext booklet {0} {1} {2}".format(
+                self.myfile, self.options.id_pattern, file_export)
+            os.system(command)
+            os.close(self.tmplog_fd)
+    
+    def do_webp(self):
+        file_export = '"' + self.options.output_dir + '"'
+        if not self.has_webp():
+            inkex.utils.errormsg("Please download and configure libwebp to do webp export")
+            return
+        command = "start inkporter_ext webp {0} {1} {2} {3}".format(
+            self.myfile, self.options.id_pattern, file_export, self.options.dpi)
+        os.system(command)
+        os.close(self.tmplog_fd)
+
+    def has_ghostscript(self):
+        status, output = self.get_cmd_output('gswin32c --help')
+        return status == 0 and 'Ghostscript' in output
+
+    def has_imagemagick(self):
+        status, output = self.get_cmd_output('convert --version')
+        return status == 0 and 'ImageMagick' in output
+
+    def has_rsvg(self):
+        status, output = self.get_cmd_output('rsvg-convert --version')
+        return status == 0 and 'rsvg-convert' in output
+    
+    def has_webp(self):
+        status, output = self.get_cmd_output('cwebp -help')
+        return status == 0 and 'output.webp' in output
+    
+    def make_tmp_file(self, file_format):
+        handler, self.myfile = tempfile.mkstemp(suffix=".svg",prefix="inkporter-%s-"%file_format)
+        with open(handler, "w") as f:
+            f.write(etree.tostring(self.document, encoding="utf-8",xml_declaration=True).decode("utf-8"))
+        self.tmpout.append(self.myfile)
+
+    def get_cmd_output(self, cmd):
+        # Adapted from webslicer extension (extensions > web > slicer)
+        if sys.platform != "win32":
+            cmd = '{ ' + cmd + '; }'
+        pipe = os.popen(cmd + ' 2>&1', 'r')
+        text = pipe.read()
+        sts = pipe.close()
+        if sts is None:
+            sts = 0
+        if text[-1:] == '\n':
+            text = text[:-1]
+        return sts, text
+
+    def do_cleanup(self):
+        # for item in self.tmpout:
+            # if os.path.exists(os.path.abspath(item)):
+                # os.remove(os.path.abspath(item))
+        # if os.path.isdir(self.tmpdir):
+            # os.rmdir(self.tmpdir)
+        print("hai")
+
+    # called when extension is running
+    def effect(self):
+        if not self.has_rsvg():
+            inkex.utils.errormsg(
+                "This extension requires rsvg-convert (from librsvg/librsvg-bin) to run, please install it before start exporting")
+            exit()
+        if len(self.options.id_pattern) > 0:
+            new_nss = inkex.utils.NSS
+            new_nss[u're'] = u'http://exslt.org/regular-expressions'
+            path_to_compile = "//*[re:match(@id,'(%s)','g')]" % self.options.id_pattern
+            self.id_to_process = self.document.xpath(path_to_compile, namespaces=new_nss)
+            self.svg.selected = {}
+            for item in self.id_to_process:
+                self.svg.selected[item.attrib['id']] = item
+        if len(self.svg.selected) < 1:
+            inkex.utils.errormsg(
+                "Please select at least 1 object or fill ID Pattern to use this extension!")
+            exit()
+        try:
+            if not os.path.isdir(os.path.expandvars(self.options.output_dir)):
+                os.mkdir(os.path.expandvars(self.options.output_dir))
+            self.make_tmp_file(self.options.format)
+            if self.options.format == "png":
+                self.do_png()
+            elif self.options.format == "jpg":
+                self.do_jpg()
+            elif self.options.format == "pdf":
+                self.do_pdf()
+            elif self.options.format == "svg":
+                self.do_svg()
+            elif self.options.format == "eps":
+                self.do_eps()
+            elif self.options.format == "booklet":
+                self.do_booklet()
+            elif self.options.format == "webp":
+                self.do_webp()
+            self.do_cleanup()
+        except Exception as e:
+            inkex.utils.errormsg(e)
+            import traceback
+            inkex.utils.errormsg(traceback.print_exc())
+            print("hai")
+            
+
+
+if __name__ == '__main__':
+    e = Inkporter()
+    e.run ()
---
+++ windows/1.0/inkporter_ext.bat
@@ -0,0 +1,186 @@
+@echo off
+setlocal
+
+:main
+rem inkporter version = 1.5
+echo "|| Welcome to                                                           ||"
+echo "||  ___       _                     _                         _         ||"
+echo "|| |_ _|_ __ | | ___ __   ___  _ __| |_ ___ _ __    __      _(_)_ __    ||"
+echo "||  | || '_ \| |/ / '_ \ / _ \| '__| __/ _ \ '__|___\ \ /\ / / | '_ \   ||"
+echo "||  | || | | |   <| |_) | (_) | |  | ||  __/ | |_____\ V  V /| | | | |  ||"
+echo "|| |___|_| |_|_|\_\ .__/ \___/|_|   \__\___|_|        \_/\_/ |_|_| |_|  ||"
+echo "||                |_|                                                   ||"
+echo Inkporter-GUI Processor for Windows
+set exdir=%4
+set target=%1
+set svgin=%2
+set objID=%3
+
+:pilihan 
+if %target%== png goto PNG
+if %target%== pdf goto PDF
+if %target%== pdf_cmyk goto PDFCMYK
+if %target%== eps goto EPS
+if %target%== svg goto SVGPLAIN
+if %target%== jpeg goto JPEG
+if %target%== webp goto WEBP
+if %target%== booklet goto BOOKLET
+if %target%== booklet_cmyk goto BOOKLETCMYK
+if %target%== bundle goto BUNDLE
+
+
+:PNG
+set dpi=%5
+:PNGBATCHPPROCESS
+echo Getting ready to export %svgin% from SVG to PNG
+
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	inkscape --export-id=%%d --export-filename=%4\%%d.png --export-dpi=%dpi%  %svgin%
+	echo File %%d.png created
+	)
+goto end
+
+:JPEG
+set dpi=%5
+set bgcol=%6
+set quality=%7
+:JPEGBATCHPPROCESS
+echo Getting ready to export %svgin% from SVG to JPEG
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	inkscape --export-id=%%d --export-filename=%4\%%d.png --export-dpi=%dpi% %svgin% >nul
+	magick convert %4\%%d.png -background %6 -flatten -quality %7 -colorspace %8 %4\%%d.jpeg
+	echo File %%d.jpeg created
+	del %4\%%d.png
+	)
+goto end
+
+:PDF
+:PDFBATCHPPROCESS
+echo Getting ready to export %svgin% from SVG to PDF
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	inkscape --export-id=%%d --export-id-only --export-plain-svg --export-filename=%4\%%d.svg %svgin%
+	inkscape --export-area-page  --export-filename=%4\%%d.pdf %4\%%d.svg
+	del %4\%%d.svg
+	echo File %%d.pdf created
+	)
+goto end
+
+:PDFCMYK
+:PDFCMYKBATCHPPROCESS
+echo Getting ready to export %svgin% from SVG to PDF dengan color space CMYK
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	inkscape --export-id=%%d --export-id-only --export-plain-svg --export-filename=%4\%%d.svg %svgin%
+	inkscape --export-area-page  --export-filename=%4\%%d-rgb.pdf %4\%%d.svg
+	gswin32c -dSAFER -dBATCH -dNOPAUSE -dNOCACHE -sDEVICE=pdfwrite -dAutoRotatePages=/None -sColorConversionStrategy=CMYK -dProcessColorModel=/DeviceCMYK -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode -dDownsampleMonoImages=false -dDownsampleGrayImages=false -sOutputFile=%4\%%d.pdf %4\%%d-rgb.pdf
+	del %4\%%d.svg
+	del %4\%%d-rgb.pdf
+	echo File %%d.pdf dengan color space CMYK created
+	)
+goto end
+
+:SVGPLAIN
+:SVGPLAINBATCHPROCESS
+echo Getting ready to export %svgin% from SVG to SVG Plain
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	inkscape --export-id=%%d --export-id-only --export-plain-svg --export-filename=%4\%%d.svg %svgin%
+	echo File %%d.svg created
+	)
+goto end
+
+:EPS
+:EPSBATCHPPROCESS
+echo Getting ready to export %svgin% from SVG to EPS
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	inkscape --export-id=%%d --export-id-only --export-plain-svg --export-filename=%4\%%d.svg %svgin%
+	inkscape %4\%%d.svg --export-filename=%4\%%d.eps --export-type=eps --export-area-page --export-ps-level=3 --export-text-to-path --export-ignore-filters >nul
+	del %4\%%d.svg
+	echo File %%d.eps created
+	)
+goto end
+
+:WEBP
+echo.
+:WEBPBATCHPPROCESS
+echo Getting ready to export %svgin% from SVG to WEBP
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	inkscape --export-id=%%d --export-filename=%4\%%d.png --export-dpi=%dpi% %svgin% >nul
+	cwebp %4\%%d.png -o %4\%%d.webp
+	echo File %%d.webp created
+	del %4\%%d.png
+	)
+goto end
+
+:BOOKLET
+set namaFile=%objID%-booklet.pdf
+:BOOKLETPPROCESS
+echo Getting ready to export %svgin% from SVG to Booklet (PDF)
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	echo Processing %%d
+	inkscape --export-id=%%d --export-id-only --export-plain-svg --export-filename=%4\%%d.svg %svgin%
+	inkscape --export-area-page --export-type=pdf --export-filename=%4\pdftemp-%%d.pdf %4\%%d.svg
+	move %4\pdftemp-%%d.pdf %4\pdftemp-%%d.pdfx
+	del %4\%%d.svg
+	)
+cd %4
+dir /b | findstr pdftemp >> %4\list.txt
+echo.
+gswin32c -sDEVICE=pdfwrite -dBATCH -dNOPAUSE -sOutputFile=%namaFile% @%4\list.txt
+del *.pdfx
+del list.txt
+goto end
+
+:BOOKLETCMYK
+set namaFile=%objID%-booklet_cmyk.pdf
+:BOOKLETCMYKPPROCESS
+echo Getting ready to export %svgin% from SVG to Booklet (PDF)
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	echo Processing %%d
+	inkscape --export-id=%%d --export-id-only --export-plain-svg --export-filename=%4\%%d.svg %svgin%
+	inkscape --export-area-page  --export-filename=%4\%%d-rgb.pdf %4\%%d.svg
+	gswin32c -dSAFER -dBATCH -dNOPAUSE -dNOCACHE -sDEVICE=pdfwrite -dAutoRotatePages=/None -sColorConversionStrategy=CMYK -dProcessColorModel=/DeviceCMYK -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode -dDownsampleMonoImages=false -dDownsampleGrayImages=false -sOutputFile=%4\%%d-temp.pdf %4\%%d-rgb.pdf
+	move %4\%%d-temp.pdf %4\pdftemp-%%d.pdfx
+	del %4\%%d-rgb.pdf
+	del %4\%%d.svg
+	)
+cd %4
+dir /b | findstr pdftemp >> %4\list.txt
+echo.
+gswin32c -sDEVICE=pdfwrite -dBATCH -dNOPAUSE -sOutputFile=%namaFile% @%4\list.txt
+del *.pdfx
+del list.txt
+goto end
+
+:BUNDLE
+set /p objID="Pola nama Object ID : "
+set /p fold="Buat folder hasil ekspor : "
+echo File akan disimpan di %cd%\%fold%
+md "%fold%" 2>nul
+set der=%cd%
+echo.
+:BUNDLEBATCHPROCESS
+echo Getting ready to export %svgin% from SVG to ZIP Bundle (PNG + EPS Default)
+for /f "delims=," %%d in ('inkscape --query-all %svgin% ^| findstr %objID%') do (
+	inkscape --export-id=%%d --export-plain-svg --export-filename=%4\%%d.svg %svgin%
+	inkscape %%d.svg --export-filename=%4\%%d.eps --export-type=eps --export-area-page --export-ps-level=3 --export-text-to-path --export-ignore-filters >nul
+	inkscape --export-id=%%d --export-filename=%4\%%d.png %svgin% >nul
+	7z a -tzip %%d.zip %%d.png %%d.eps
+	del %%d.svg
+	del %%d.png
+	del %%d.eps
+	move %%d.zip "%der%\%fold%\" >nul
+	echo File %%d.zip created
+	)
+goto end
+
+:end
+echo.
+REM echo Permintaan anda telah diselesaikan
+REM echo File anda telah disimpan di %cd%\%fold%
+REM echo tekan enter untuk keluar
+del %2
+rem pause >nul
+
+:langsung_end
+endlocal
+exit
+:void
